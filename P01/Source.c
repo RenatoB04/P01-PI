8bf1b660e3eead79d14d3a604f22468a287e0fea
Renato <47258416+RenatoB04@users.noreply.github.com>
2023-11-24 13:40:14 +0000

    Fixed Warnings. Changed board

--- P01/Source.c
+++ P01/Source.c
@@ -54,32 +54,52 @@ void displayMainMenu() {
 void displayGameGrid() {
     printf("\n=== Middle-Earth Game Grid ===\n");
 
-    printf("   ");
-    for (char col = 'A'; col <= 'Z'; col++) {
-        printf(" %c  ", col);
+    printf("   A B C D E F G H I J K L M N O P Q R J T U V W X Y Z\n");
+    printf("  %c", 218);
+    for (int i = 0; i < 25; i++)
+    {
+        printf("%c%c", 196, 194);
     }
-    printf("\n");
-
-    for (int row = 0; row <= 16; row++) {
-        printf("%2d ", row);
-
-        for (char col = 'A'; col <= 'Z'; col++) {
-            printf("+---");
+    printf("%c%c\n", 196, 191);
+    for (int ii = 0; ii < 10; ii++)
+    {
+        printf("%d ", ii);
+        for (int i = 0; i < 27; i++)
+        {
+            printf("%c ", 179);
         }
-        printf("+\n");
-
-        printf("   ");
-        for (char col = 'A'; col <= 'Z'; col++) {
-            printf("|   ");
+        printf("\n  %c", 195);
+        for (int i = 0; i < 25; i++)
+        {
+            printf("%c%c", 196, 197);
         }
-        printf("|\n");
+        printf("%c%c\n", 196, 180);
     }
-
-    printf("   ");
-    for (char col = 'A'; col <= 'Z'; col++) {
-        printf("+---");
+    for (int ii = 10; ii < 17; ii++)
+    {
+        printf("%d", ii);
+        for (int i = 0; i < 27; i++)
+        {
+            printf("%c ", 179);
+        }
+        printf("\n  %c", 195);
+        for (int i = 0; i < 25; i++)
+        {
+            printf("%c%c", 196, 197);
+        }
+        printf("%c%c\n", 196, 180);
     }
-    printf("+\n");
+    printf("17");
+    for (int i = 0; i < 27; i++)
+    {
+        printf("%c ", 179);
+    }
+    printf("\n  %c", 192);
+    for (int i = 0; i < 25; i++)
+    {
+        printf("%c%c", 196, 193);
+    }
+    printf("%c%c", 196, 217);
 
     printf("\nOptions:\n");
     printf("1. Place Buildings\n2. Place Unit\n3. Move Unit\n4. Attack with Unit\n5. Current Amount of Coins\n6. End Turn\n");
@@ -116,20 +136,21 @@ void displayGameGrid() {
 void startNewGame() {
     char playerChoice[20];
     printf("\n=== New Game Setup ===\nChoose your side:\n1. Gondor/Rivendell\n2. Mordor\nEnter your choice: ");
-    scanf_s("%c", playerChoice);
+    if (scanf_s("%19s", playerChoice, (unsigned int)sizeof(playerChoice)) == 1) {
+        if (playerChoice[0] == '2' || (playerChoice[0] == 'M' && playerChoice[1] == 'o' && playerChoice[2] == 'r' && playerChoice[3] == 'd' && playerChoice[4] == 'o' && playerChoice[5] == 'r')) {
+            printf("You have chosen to play as Mordor.\n");
+        }
+        else {
+            printf("You have chosen to play as Gondor/Rivendell.\n");
+        }
 
-    getchar();
-
-    if (playerChoice[0] == '2' || (playerChoice[0] == 'M' && playerChoice[1] == 'o' && playerChoice[2] == 'r' && playerChoice[3] == 'd' && playerChoice[4] == 'o' && playerChoice[5] == 'r')) {
-        printf("You have chosen to play as Mordor.\n");
+        displayGameGrid();
+        printf("Starting the new game...\n");
     }
     else {
-        printf("You have chosen to play as Gondor/Rivendell.\n");
+        printf("Error reading player choice.\n");
+        exit(EXIT_FAILURE);
     }
-
-    displayGameGrid();
-
-    printf("Starting the new game...\n");
 }
 
 void loadGame() {
